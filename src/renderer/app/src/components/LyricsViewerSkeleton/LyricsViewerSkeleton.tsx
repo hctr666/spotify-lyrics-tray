8b4140904c7e49dbb71d8cb7afd65bc1eb70330d
hctr666 <hctr.441@gmail.com>
2023-06-23 21:24:08 -0500

    chore: improve lyrics viewer skeleton

--- src/renderer/app/src/components/LyricsViewerSkeleton/LyricsViewerSkeleton.tsx
+++ src/renderer/app/src/components/LyricsViewerSkeleton/LyricsViewerSkeleton.tsx
@@ -14,21 +14,21 @@ const initialRect = {
   width: getRandomWidth(),
 }
 
-const rects = [...Array(10).keys()].reduce(
-  (acc, _) => {
-    const last = acc[acc.length - 1]
-    return [...acc, { ...last, y: last.y + GAP, width: getRandomWidth() }]
-  },
-  [initialRect]
-)
-
 export const LyricsViewerSkeleton = () => {
+  const rects = [...Array(10).keys()].reduce(
+    (acc, _) => {
+      const last = acc[acc.length - 1]
+      return [...acc, { ...last, y: last.y + GAP, width: getRandomWidth() }]
+    },
+    [initialRect]
+  )
+
   return (
     <ContentLoader
       viewBox='0 0 300 285'
       backgroundOpacity={0.12}
       foregroundOpacity={0.2}
-      style={{ overflow: 'hidden', padding: '2rem 2.4rem' }}
+      style={{ overflow: 'hidden', padding: '2rem 2rem' }}
     >
       {rects.map(rectProps => (
         <rect key={`rect-${rectProps.y}`} {...rectProps} />
